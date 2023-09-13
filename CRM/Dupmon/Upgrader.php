d13d954f3194cd779d0da8b1f42e78ce6c23c164
Allen Shaw <allen@JoineryHQ.com>
2023-09-13 01:44:49 -0500

    civix upgrade (civix v 23.02.1)

--- CRM/Dupmon/Upgrader.php
+++ CRM/Dupmon/Upgrader.php
@@ -8,7 +8,7 @@ use CRM_Dupmon_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Dupmon_Upgrader extends CRM_Dupmon_Upgrader_Base {
+class CRM_Dupmon_Upgrader extends CRM_Extension_Upgrader_Base {
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
 
--- dupmon.php
+++ dupmon.php
@@ -53,59 +53,12 @@ function dupmon_civicrm_install(): void {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function dupmon_civicrm_postInstall(): void {
-  _dupmon_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function dupmon_civicrm_uninstall(): void {
-  _dupmon_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function dupmon_civicrm_enable(): void {
   _dupmon_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function dupmon_civicrm_disable(): void {
-  _dupmon_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function dupmon_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _dupmon_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function dupmon_civicrm_entityTypes(&$entityTypes): void {
-  _dupmon_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
